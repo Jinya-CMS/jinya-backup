4be377d0db1b528fae5f0e5bcae5f02c1adb6fcc
Imanuel Ulbricht <me@imanuel.dev>
2022-07-20 15:16:25 +0200

    TG-20 Replace handlebars with jinya-html
    Added jinya-html for import/export page

--- web/js/importExport.js
+++ web/js/importExport.js
@@ -1,9 +1,23 @@
 import {pageBody, resetContent} from "./utils.js";
+import {html} from "../lib/js/jinya-html.js";
 
 export async function displayImportExport() {
     resetContent();
-    const template = Handlebars.compile(document.getElementById('importExportTemplate').innerHTML);
-    pageBody.innerHTML = template();
+    pageBody.innerHTML = html`
+        <div class="cosmo-page-body__content">
+            <h3>Export your current database</h3>
+            <div>
+                <a href="/api/export" target="_blank" class="cosmo-button" id="exportDatabase">Export</a>
+            </div>
+            <h3>Import backup</h3>
+            <div class="cosmo-input__group">
+                <label for="filePicker" class="cosmo-label">Pick backup file</label>
+                <input id="filePicker" type="file" class="cosmo-input">
+            </div>
+            <div class="cosmo-button__container">
+                <button class="cosmo-button" id="importDatabase">Import</button>
+            </div>
+        </div>`;
 
     document.getElementById('importDatabase').addEventListener('click', () => {
         const files = document.getElementById('filePicker').files;
